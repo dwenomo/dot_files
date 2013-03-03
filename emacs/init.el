59cd4ec4dcd3bf40093e6c96710d012746abcec7
dwenomo <dwenomo@gmail.com>
2013-03-04 02:28:40 +0900

    edit init.el and display

--- emacs/init.el
+++ emacs/init.el
@@ -11,34 +11,6 @@
 (global-linum-mode 1)
 (setq scroll-step 1)
 
- ;; 見た目			     	
-(custom-set-variables
-;; '(ansi-color-names-vector ["#212526" 
-;;  			    "#ff4b4b"
-;;  			    "#b4fa70"
-;;  			    "#fce94f"
-;;  			    "#729fcf"
-;;  			    "#ad7fa8"
-;;  			    "#8cc4ff"
-;;  			    "#eeeeec"])
- '(column-number-mode t)
- '(current-language-environment "UTF-8")
- '(custom-enabled-themes (quote (wheatgrass)))
- '(display-battery-mode t)
- '(display-time-mode t)
- '(size-indication-mode t)
- '(tool-bar-mode nil))
-;;  
-;;(custom-set-faces
-;; '(default ((t (:family #("ＭＳ ゴシック" 0 7 (charset cp932-2-byte)) 
-;;                :foundry "outline" 
-;;                :slant normal 
-;;                :weight normal 
-;;                :height 90 
-;;                :width normal)))))
-;;  
-(set-frame-parameter nil 'alpha '95)
-
  ;; パッケージ
 (require 'package)
 (add-to-list 'package-archives 
--- emacs/inits/00_display.el
+++ emacs/inits/00_display.el
@@ -1,1 +1,22 @@
+ ;; 見た目			     	
+(custom-set-variables
+ '(column-number-mode t)
+ '(current-language-environment "UTF-8")
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(display-battery-mode t)
+ '(display-time-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+
+;; フォント周り
+(custom-set-faces
+ '(default ((t (:family #("ＭＳ ゴシック" 0 7 (charset cp932-2-byte)) 
+		:foundry "outline" 
+		:slant normal 
+		:weight normal 
+		:height 90 
+		:width normal)))))
+  
+(set-frame-parameter nil 'alpha '95)
+
 (load-theme 'solarized-dark t)
