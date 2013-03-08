5df18ffe8962102746a0fa6470c895661b75e8b7
dwenomo <dwenomo@gmail.com>
2013-03-09 04:01:43 +0900

    add helm all

---
+++ emacs/inits/02_helm.el
@@ -0,0 +1,5 @@
+;; https://github.com/emacs-helm/helm
+;; http://emacs.tsutomuonoda.com/emacs-anything-el-helm-mode-install/
+(require 'helm-config)
+(global-set-key (kbd "C-c h") 'helm-mini)
+(helm-mode 1)
