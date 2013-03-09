9eac57f36c89213574e232c944a2c07f43eebaec
dwenomo <dwenomo@gmail.com>
2013-03-10 03:50:28 +0900

    add el-get

--- emacs/init.el
+++ emacs/init.el
@@ -28,14 +28,3 @@
 
 (require 'init-loader)
 (init-loader-load (concat user-emacs-directory "inits"))
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(el-get 'sync)
---
+++ emacs/inits/06_el-get.el
@@ -0,0 +1,13 @@
+(add-to-list 'load-path (concat user-emacs-directory "el-get/el-get"))
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync
+	'(el-get
+	  emacs-w3m
+	  ))
