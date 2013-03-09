75d56f654c7db4706dc9dddf69366e95e52c60fe
dwenomo <dwenomo@gmail.com>
2013-03-10 03:03:38 +0900

    add php-mode open-junk-file add gitignore emacs/url

---
+++ emacs/inits/20_php-mode.el
@@ -0,0 +1,6 @@
+(add-hook 'php-mode-hook
+          (lambda ()
+            (c-set-offset 'case-label' 4)
+            (c-set-offset 'arglist-intro' 4)
+            (c-set-offset 'arglist-cont-nonempty' 4)
+            (c-set-offset 'arglist-close' 0)))
